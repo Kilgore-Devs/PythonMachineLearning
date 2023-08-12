dc6a762639d9688a943b6fe2469905b71259c122
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-08-12 10:25:45 -0500

    Standard deviation and variance.

--- Basics/Standard Deviation.py
+++ Basics/Standard Deviation.py
@@ -7,3 +7,18 @@ age = [55, 22, 33, 44, 18, 62, 40, 25, 29]
 x = numpy.std(age)
 
 print(x)
+
+# Variance is another number that shows how spread out values are.
+# SD^2 = variance or sqrt of variance is SD.
+# to find the variance -  find the mean(avg of set of nums)
+# then find the diff from mean
+# for each diff, square it
+# then find the average of the squared diffs
+age = [55, 22, 33, 44, 18, 62, 40, 25, 29]
+y = numpy.var(age)
+# detail of SD = sqrt of var
+z = numpy.sqrt(y)
+print(y)
+# detail of SD = sqrt of var
+print("Standard deviation is equal to the square rooot of variation.\n"
+      "In this case, the variance is: " + str(y) + ". So the standard deviation is: " + str(z) + ".")
